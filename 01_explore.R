63d0dabc6edc98d232fb17c05f6791dcf9329a77
Quang Nguyen <43119108+qntkhvn@users.noreply.github.com>
2021-11-16 20:24:49 -0600

    some exploratory work

---
+++ 01_explore.R
@@ -0,0 +1,29 @@
+# 3 methods
+# read.csv (from base)
+# read_csv (from readr/tidyverse)
+# fread    (from data.table)
+
+# slow... took forever
+r1 <- read.csv("df.csv")
+
+# it's really the question of which of the remaining 2 is faster?
+
+# read_csv
+time2 <- rep(NA, nsim)
+for (i in 1:nsim) {
+  start <- Sys.time()
+  r2 <- readr::read_csv("df.csv")
+  time2[i] <- Sys.time() - start
+}
+hist(time2)
+summary(time2)
+
+# fread
+time3 <- rep(NA, nsim)
+for (i in 1:nsim) {
+  start <- Sys.time()
+  r3 <- data.table::fread("df.csv")
+  time3[i] <- Sys.time() - start
+}
+hist(time3)
+summary(time3)
