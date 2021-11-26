9d4094d7c37fff745bd9816526367cfcb593d04f
Quang Nguyen <ngminhquang.99@gmail.com>
2021-11-26 00:17:12 -0600

    final file sizes

--- 00_fake_data.R
+++ 00_fake_data.R
@@ -13,10 +13,10 @@ head(df)
 # write_csv(df, "df.csv")
 
 
-x2 <- matrix(nrow = 750000, ncol = 100)
+x2 <- matrix(nrow = 252000, ncol = 100)
 set.seed(101)
 for(i in 1:ncol(x2)) {
-  x2[, i] <- rnorm(750000)
+  x2[, i] <- rnorm(252000)
 }
 
 df2 <- as_tibble(x2)
@@ -27,7 +27,7 @@ write_csv(df2, "df2.csv")
 
 
 
-x3 <- matrix(nrow = 1000000, ncol = 55)
+x3 <- matrix(nrow = 1000000, ncol = 62)
 set.seed(102)
 for(i in 1:ncol(x3)) {
   x3[, i] <- rnorm(1000000)
@@ -42,9 +42,14 @@ write_csv(df3, "df3.csv")
 
 file.info("df.csv")[1] # 1.963204005 GB
 
-file.info("df2.csv")[1] # 1.472402481 GB
+file.info("df2.csv")[1] # 0.494723378 GB
 
-file.info("df3.csv")[1] # 1.079761146 GB
+file.info("df3.csv")[1] # 1.217186978 GB
 
 
 
+# file size
+
+# low: 0.5 GB
+# medium: 1 GB
+# high: 2 GB
