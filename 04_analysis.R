9eaad1d9a240d3a66d07cea5a52fae8a5233d9cc
Quang Nguyen <ngminhquang.99@gmail.com>
2021-11-30 15:28:33 -0600

    anova

--- 04_analysis.R
+++ 04_analysis.R
@@ -3,3 +3,14 @@ res <- bind_rows(read_csv("res_quang.csv"),
                  read_csv("res_lance.csv"))
 
 res %>% count(computer)
+
+
+res %>% 
+  ggplot(aes(time, package)) +
+  geom_boxplot() +
+  facet_grid(~ size)
+
+
+mod <- lm(time ~ computer*size*package, data = res)
+mod %>% 
+  anova()
