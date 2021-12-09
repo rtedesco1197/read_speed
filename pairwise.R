5cc6993bc6b502ad2f84ae8aecb9c20158f09c2a
Quang Nguyen <ngminhquang.99@gmail.com>
2021-12-08 22:27:12 -0600

    FDR pairwise

--- pairwise.R
+++ pairwise.R
@@ -1,6 +1,6 @@
 install.packages("rcompanion")
 library(rcompanion)
 
-pairwisePermutationTest(time ~ size, data = res)
-pairwisePermutationTest(time ~ package, data = res, method = "bonf")
-pairwisePermutationTest(time ~ computer, data = res, method = "bonf")
+pairwisePermutationTest(time ~ size, data = res, method = "fdr")
+pairwisePermutationTest(time ~ package, data = res, method = "fdr")
+pairwisePermutationTest(time ~ computer, data = res, method = "fdr")
